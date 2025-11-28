bdc5886131cb7f6649cce6a7a99ff4bc6fbb7f2b
rifathosenofficial2023 <rifathosenofficial@gmail.com>
2025-11-28 08:25:54 +0600

    addedc

--- lib/NavViews/nav_bar_views.dart
+++ lib/NavViews/nav_bar_views.dart
@@ -1,4 +1,6 @@
+import 'package:e_learning_ui/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class NavBarViews extends StatefulWidget {
   const NavBarViews({super.key});
@@ -8,11 +10,32 @@ class NavBarViews extends StatefulWidget {
 }
 
 class _NavBarViewsState extends State<NavBarViews> {
+
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [
-        
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: AppColors.primaryColor,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        items: [
+        BottomNavigationBarItem(icon: Icon(Icons.star_border),
+         label: 'Featured'),
+        BottomNavigationBarItem(icon: Icon(Icons.play_circle_outline),
+         label: 'Learning'),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite_border),
+         label: 'Wishlist'),
+        BottomNavigationBarItem(icon: Icon(Icons.settings),
+         label: 'Setting'),
+
       ]),
     );
   }
