2037ecb6ffdd830d9b190a125442f6113e5680cb
rifathosenofficial2023 <rifathosenofficial@gmail.com>
2025-11-28 10:39:46 +0600

    added

--- lib/NavViews/nav_bar_views.dart
+++ lib/NavViews/nav_bar_views.dart
@@ -1,6 +1,7 @@
 import 'package:e_learning_ui/utils/colors.dart';
+import 'package:e_learning_ui/views/HomeScreen/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+//import 'package:flutter_svg/svg.dart';
 
 class NavBarViews extends StatefulWidget {
   const NavBarViews({super.key});
@@ -12,6 +13,19 @@ class NavBarViews extends StatefulWidget {
 class _NavBarViewsState extends State<NavBarViews> {
 
   int currentIndex = 0;
+  List <Widget> screens = [
+    const HomeScreen(),
+   const Center(
+      child: Text('My Learning'),
+    ),
+   const Center(
+      child: Text('Wishlist'),
+    ),
+   const Center(
+      child: Text('Settings'),
+    ),
+
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +35,14 @@ class _NavBarViewsState extends State<NavBarViews> {
         unselectedItemColor: Colors.black,
         showUnselectedLabels: true,
         currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
         onTap: (value) {
           setState(() {
             currentIndex = value;
           });
         },
         items: [
-        BottomNavigationBarItem(icon: Icon(Icons.star_border),
+        BottomNavigationBarItem(activeIcon: Icon(Icons.star), icon: Icon(Icons.star_border),
          label: 'Featured'),
         BottomNavigationBarItem(icon: Icon(Icons.play_circle_outline),
          label: 'Learning'),
